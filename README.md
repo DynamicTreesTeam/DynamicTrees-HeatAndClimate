88d0a2afaf3c78b17b007b01edba57272c87472e
Harleyoc1 <harley.oconnor@icloud.com>
2020-09-18 20:27:22 +0100

    Added README.md.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dynamic Trees for Heat and Climate
+Dynamic Trees compatibility add-on for Heat and Climate currently in the works. This is a fork of [odorousrex's](https://github.com/odorousrex) [original project](https://www.curseforge.com/minecraft/mc-mods/dynamic-trees-heat-and-climate-compat), as it no longer works with newer versions of Dynamic Trees.
+
+## Supported Trees
+- Lemon
+- Morus
+- Olive
--- src/main/java/com/harleyoconnor/dynamictreeshnc/AddonContent.java
+++ src/main/java/com/harleyoconnor/dynamictreeshnc/AddonContent.java
@@ -24,13 +24,11 @@ import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * @author Harley O'Connor
